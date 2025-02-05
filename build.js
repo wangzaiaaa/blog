97f5bafc584e4018c50f09b59082149c5133c190
wangzaiaaa <2627894288@qq.com>
2025-02-05 20:23:09 +0800

    5

--- build.js
+++ build.js
@@ -2,6 +2,24 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
+// 递归处理目录中的所有.txt文件
+function processDirectory(dir) {
+  const items = fs.readdirSync(dir);
+  
+  for (const item of items) {
+    const fullPath = path.join(dir, item);
+    const stat = fs.statSync(fullPath);
+    
+    if (stat.isDirectory()) {
+      processDirectory(fullPath);
+    } else if (stat.isFile() && item.endsWith('.txt')) {
+      const newPath = fullPath.replace(/\.txt$/, '.html');
+      fs.renameSync(fullPath, newPath);
+      console.log(`Renamed: ${fullPath} -> ${newPath}`);
+    }
+  }
+}
+
 // Run the Next.js build
 execSync('next build', { stdio: 'inherit' });
 
@@ -10,6 +28,9 @@ const outDir = path.join(process.cwd(), 'out');
 if (!fs.existsSync(outDir)) {
   fs.mkdirSync(outDir, { recursive: true });
 }
+
+// 处理构建输出目录中的.txt文件
+processDirectory(outDir);
 
 // 复制_routes.json到out目录
 fs.copyFileSync(
