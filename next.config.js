882a1cafe30e7afbb5e6c9f6fd1040cba3e6b470
wangzaiaaa <2627894288@qq.com>
2025-02-05 19:39:29 +0800

    2

--- next.config.js
+++ next.config.js
@@ -9,6 +9,8 @@ const nextConfig = {
     unoptimized: true, // 禁用图片优化
   },
   trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://blog.keepdev.fun' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
 };
 
 module.exports = nextConfig;
